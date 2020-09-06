0313c6a90bc16a98f3ef9b38508744e99e850885
monica mota ocariz <mota.ocariz@gmail.com>
2020-09-06 18:01:57 -0300

    :fire: Unused files removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Course Project`,
+    description: `This project is a result of the course gatsby.`,
+    author: `@mocariz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
