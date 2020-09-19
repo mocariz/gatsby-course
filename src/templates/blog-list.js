18556f492b2611d1463bee56a8d4dc1fc436bbfb
Monica Mota Ocariz <mota.ocariz@gmail.com>
2020-09-19 16:22:10 -0300

    :sparkles: New page template blog-list

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,39 +1,16 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
-const IndexPage = () => {
-  const { allMarkdownRemark } = useStaticQuery(graphql`
-    query PostList {
-      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              background
-              category
-              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-              description
-              title
-            }
-            timeToRead
-          }
-        }
-      }
-    }
-  `)
-
-  const postList = allMarkdownRemark.edges
+const BlogList = props => {
+  const postList = props.data.allMarkdownRemark.edges
 
   return (
     <Layout>
       <SEO title="Home" />
-      
       {postList.map(
         ({
           node: {
@@ -53,9 +30,34 @@ const IndexPage = () => {
           />
         )
       )}
-
     </Layout>
   )
 }
 
-export default IndexPage
+export const query = graphql`
+  query PostList($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            background
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            title
+          }
+          timeToRead
+        }
+      }
+    }
+  }
+`
+
+export default BlogList
